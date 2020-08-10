bf954c97149c69f85aca9e6464115bbf4fd5b45e
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2020-08-10 01:19:56 +0100

    day2 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -22,9 +22,24 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        # address = 0
+        address = 0
 
         # # For now, we've just hardcoded a program:
+
+        program = [
+            0b10000010,  # LDI R0,8
+            0b00000000,
+            0b00001000,
+            0b10000010,  # LDI R1,9
+            0b00000001,
+            0b00001001,
+            0b10100010,  # MUL R0,R1
+            0b00000000,
+            0b00000001,
+            0b01000111,  # PRN R0
+            0b00000000,
+            0b00000001,  # HLT
+        ]
 
         # program = [
         #     # From print8.ls8
@@ -36,25 +51,26 @@ class CPU:
         #     0b00000001,  # HLT
         # ]
 
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
+        for instruction in program:
+            self.ram[address] = instruction
+            address += 1
 
-        filename = sys.argv[1]
-        try:
-            address = 0
-            with open(filename) as f:
-                for line in f:
-                    line = line.split('#')[0]
-                    command = line.strip()
-                    if command == '':
-                        continue
-                    instruction = int(command, 2)
-                    self.ram_write(address, instruction)
-                    address += 1
-        except FileNotFoundError:
-            print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
-            sys.exit()
+        # filename = sys.argv[1]
+        # try:
+        #     address = 0
+        #     with open(filename) as f:
+        #         for line in f:
+        #             line = line.split('#')[0]
+        #             command = line.strip()
+        #             if command == '':
+        #                 continue
+        #             instruction = int(command, 2)
+        #             self.ram_write(address, instruction)
+
+        #             address += 1
+        # except FileNotFoundError:
+        #     print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
+        #     sys.exit()
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -62,6 +78,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -96,6 +114,7 @@ class CPU:
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
 
         running = True
 
@@ -116,3 +135,7 @@ class CPU:
 
             if IR == HLT:
                 running = False
+
+            if IR == MUL:
+                self.alu('MUL', operand_a, operand_b)
+                self.pc += 3
